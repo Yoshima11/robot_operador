23e3c6a97cd062c1504c5fb54cb20d08ba156420
Yoshima11 <miked160581@gmail.com>
2023-11-12 02:34:33 -0300

    toma datos de la linea de comando y se conecta

--- robot_operador.py
+++ robot_operador.py
@@ -1,39 +1,42 @@
+import sys
+import argparse #para ingresar datos por linea de comando
 import time
 from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 import pyRofex as pr
 
-def login():
-    print('Autenticación a Remarkets')
-    user = input('Ingrese el usuario:')
-    cuenta = input('Ingrese la cuenta:')
-    password = input('Ingrese el password:')
-    real = input('Entorno RAMARKET o LIVE (r/l)')
-    if(real=='l' or real=='L'):
-        try:
-            resp = pr.initialize(
-                user=user,
-                password=password,
-                account=cuenta,
-                environment=pr.Environment.LIVE,
-                active_token=token
-            )
-            print('Conexión establecida.')
-        except:
-            print('Fallo de autenticación. User o Password incorectos')
-            return
-    else:
-        try:
-            resp = pr.initialize(
-                user=user,
-                password=password,
-                account=cuenta,
-                environment=pr.Environment.REMARKET
-            )
-            print('Conexión establecida.')
-        except:
-            print('Fallo de autenticación. User o Password incorectos')
-            return
+# recibe argumentos al ejecutar el programa.
+parser = argparse.ArgumentParser(description='Robot de trading para Matba Rofex')
+parser.add_argument('-t','--ticker')
+parser.add_argument('-u','--user')
+parser.add_argument('-a','--account')
+parser.add_argument('-p','--password')
+parser.add_argument('-e','--environment', help='Environment LIVE or REMARKET')
+args = parser.parse_args()
 
-login()
+#se conecta, o no.
+if(str(args.environment).upper()=='LIVE'): 
+    try:
+        pr.initialize(
+            user=args.user,
+            password=args.password,
+            account=args.account,
+            environment=pr.Environment.LIVE,
+        )
+        print('Conexión establecida en el entorno LIVE.')
+    except Exception as error:
+        print('Fallo de autenticación del entorno LIVE.')
+        print(error)
+else:
+    try:
+        pr.initialize(
+            user=args.user,
+            password=args.password,
+            account=args.account,
+            environment=pr.Environment.REMARKET,
+        )
+        print('Conexión establecida en el entorno REMARKET.')
+    except Exception as error:
+        print('Fallo de autenticación del entorno REMARKET.')
+        print(error)
