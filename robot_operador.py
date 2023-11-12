545497ef62a5773d27d2fedc2318a2960bb38bb9
Yoshima11 <miked160581@gmail.com>
2023-11-12 04:54:47 -0300

    cutre conexión

--- robot_operador.py
+++ robot_operador.py
@@ -1,5 +1,5 @@
 import sys
-import argparse #para ingresar datos por linea de comando
+import argparse #para ingresar argumentos al iniciar el programa
 import time
 from datetime import datetime
 import pandas as pd
@@ -12,7 +12,7 @@ parser.add_argument('-t','--ticker')
 parser.add_argument('-u','--user')
 parser.add_argument('-a','--account')
 parser.add_argument('-p','--password')
-parser.add_argument('-e','--environment', help='Environment LIVE or REMARKET')
+parser.add_argument('-e','--environment', help='Environment LIVE or REMARKET. Por defecto es REMARKET')
 args = parser.parse_args()
 
 #se conecta, o no.
@@ -40,3 +40,37 @@ else:
     except Exception as error:
         print('Fallo de autenticación del entorno REMARKET.')
         print(error)
+
+def market_data_handler(message):
+    print("Market Data Message Received: {0}".format(message))
+    global mensaje
+    mensaje = message
+def order_report_handler(message):
+    print("Order Report Message Received: {0}".format(message))
+def error_handler(message):
+    print("Error Message Received: {0}".format(message))
+def exception_handler(e):
+    print("Exception Occurred: {0}".format(e.message))
+
+# Initiate Websocket Connection
+pr.init_websocket_connection(
+    market_data_handler=market_data_handler,
+    order_report_handler=order_report_handler,
+    error_handler=error_handler,
+    exception_handler=exception_handler,
+)
+
+datos_ticker = [
+    pr.MarketDataEntry.BIDS,
+    pr.MarketDataEntry.OFFERS,
+    pr.MarketDataEntry.LAST,
+]
+lista_tickers = [args.ticker]
+
+pr.market_data_subscription(
+    tickers=lista_tickers,
+    entries=datos_ticker,
+)
+
+input('apreta enter')
+pr.close_websocket_connection()
