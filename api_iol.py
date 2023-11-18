0afee8b85b04f5160ba3f84c12c9115b0f840f42
Yoshima11 <miked160581@gmail.com>
2023-11-18 05:37:33 -0300

    eliminado datos.py

--- api_iol.py
+++ api_iol.py
@@ -5,14 +5,12 @@ import urls
 
 def connect_iol(user, password):
     """Pide el token a iol"""
-    try:
-        data = {
-            "username": user,
-            "password": password,
-            "grant_type": "password",
-        }
-        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-        ret = requests.post(url=urls.token, data=data, headers=headers, timeout=10)
-        return json.loads(ret.text)
-    except:
-        print(ret.text)
+    data = {
+        "username": user,
+        "password": password,
+        "grant_type": "password",
+    }
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    ret = requests.post(url=urls.token, data=data, headers=headers, timeout=10)
+    return json.loads(ret.text)
+
--- datos.py
+++
@@ -1,3 +0,0 @@
-user = 'eduardomichel1118746'
-account = 'REM8746' 
-password = 'zgjyaO7@'
