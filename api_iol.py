de35e1428099e8cfabf0a676d148971b36a60fc9
Yoshima11 <miked160581@gmail.com>
2023-11-19 15:47:41 -0300

    iol acces y refresh token

--- api_iol.py
+++ api_iol.py
@@ -1,14 +1,22 @@
-'''json'''
-import json
+'''api requests'''
 import requests
 import urls
 
-def connect_iol(user, password):
-    """Pide el token a iol"""
+def req_token(user, password):
+    """request token iol"""
     data = {
         "username": user,
         "password": password,
         "grant_type": "password",
+    }
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    ret = requests.post(url=urls.token, data=data, headers=headers, timeout=10)
+    return ret
+def ref_token(refresh_token):
+    '''Refresh token iol'''
+    data = {
+        "refresh_token": refresh_token,
+        "grant_type": 'refresh_token',
     }
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     ret = requests.post(url=urls.token, data=data, headers=headers, timeout=10)
--- robot_operador.py
+++ robot_operador.py
@@ -1,4 +1,6 @@
 '''flet módulo para crear ventanas'''
+import threading
+import time
 import json
 import flet as ft
 import api_iol
@@ -9,47 +11,76 @@ refresh_token = ''
 def main(page: ft.Page):
     '''Ventana de la app'''
     page.title = 'Robot Operador'
-    dlg = ft.AlertDialog(
-        title=ft.Text("Request token error: User or password incorrect.")
+    dlg_error = ft.AlertDialog(
+        title=ft.Text("Request token error: invalid username or password."),
     )
-    def connect_iol(e):
-        global access_token
-        global refresh_token
-        res = api_iol.connect_iol(user.value, password.value)
+    def dis_log_controls():
+        '''Desactiva los campos del login una vez conseguido el access token'''
         user.value = ''
         password.value = ''
+        user.disabled = True
+        password.disabled = True
+        get_token.disabled = True
+        token_status.value = '---[IOL token request accepted.]---'
+        token_status.color = ft.colors.GREEN
         page.update()
-        if(res.status_code==200):
-            user.disabled = True
-            password.disabled = True
-            get_token.disabled = True
-            token_status.value = 'IOL token request accepted.'
-            token_status.color = ft.colors.GREEN
-            page.update()
+    def refresh_iol():
+        '''Cada 15 min actualiza token'''
+        global access_token
+        global refresh_token
+        while True:
+            time.sleep(900)
+            res = api_iol.ref_token(refresh_token)
+            tokens = res.json()
+            access_token = tokens['access_token']
+            refresh_token = tokens['refresh_token']
+    def access_iol(e):
+        '''solicita token por primera vez'''
+        global access_token
+        global refresh_token
+        res = api_iol.req_token(user.value, password.value)
+        if res.status_code==200: #200: solicitud token aceptada.
+            dis_log_controls()
             token = res.json()
             access_token = token['access_token']
             refresh_token = token['refresh_token']
-        else:
-            token_status.value = 'IOL token request rejected.'
-            token_status.color = ft.colors.RED
-            page.dialog = dlg
-            dlg.open = True
             page.update()
+            refresh = threading.Thread(target=refresh_iol) #solicitar refresh token cada 15 min.
+            refresh.start()
+        else: #401 es el código si los datos no son correctos.
+            token_status.value = '---[IOL token request rejected.]---'
+            token_status.color = ft.colors.RED
+            page.dialog = dlg_error
+            dlg_error.open = True
+            page.update()
+
     user = ft.TextField(label='user name:')
     password = ft.TextField(label='user password', password=True, can_reveal_password=True)
-    get_token = ft.ElevatedButton(text='request iol token', on_click=connect_iol)
+    get_token = ft.ElevatedButton(text='request iol token', on_click=access_iol)
     token_status = ft.Text()
+    mensaje = ft.Text()
     row1 = ft.Row(
         controls=[
             user,
             password,
             get_token,
+        ],
+    )
+    row2 = ft.Row(
+        controls=[
             token_status,
+        ]
+    )
+    row3 = ft.Row(
+        controls=[
+           mensaje,
         ],
     )
     col = ft.Column(
         controls=[
             row1,
+            row2,
+            row3,
         ],
     )
     page.add(
